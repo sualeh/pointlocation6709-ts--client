34a3d0673be7ba5b811166ddb7aac7c02092fc73
Sualeh Fatehi <sualeh@hotmail.com>
2018-04-12 22:30:15 -0400

    Adding support for v0.3.0

--- src/main/typescript/pointlocation6709--client.ts
+++ src/main/typescript/pointlocation6709--client.ts
@@ -1,17 +1,18 @@
 import { PointLocation6709 } from 'pointlocation6709-ts'
 
-function usePointLocation6709() 
-{
-	
+function usePointLocation6709() {
+
     let angle1 = new PointLocation6709.Angle(1);
-	console.log(angle1.toString());
+    console.log("Angle: " + angle1.toString());
 
     let latitude1 = new PointLocation6709.Latitude(angle1);
-	console.log(latitude1.toString());
-	
+    console.log("Latitude: " + latitude1.toString());
+
     let longitude1 = new PointLocation6709.Longitude(angle1);
-	console.log(longitude1.toString());
-	
+    console.log("Longitude: " + longitude1.toString());
+
+    let pointlocation1 = new PointLocation6709.PointLocation(latitude1, longitude1, 1);
+    console.log("Point Location: " + pointlocation1.toString());
 };
 
 usePointLocation6709();
